8d0d01652a40b896e95e2f2de50fad48105c903b
DhvanishChodavadiya <155216549+DhvanishChodavadiya@users.noreply.github.com>
2024-06-08 12:02:02 +0530

    api response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 // require('dotenv').config({path:'./env'})
-import dbConnect from './db/index.js';
+import dbConnect from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
-dbConnect();
+dbConnect()
+  .then(() => {
+    app.listen(process.env.PORT || 6000, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed !! ", error);
+  });
